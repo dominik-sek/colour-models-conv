c67a1371d4f602a2e8a670cc774a85fab1544edd
gothic459 <d.sek464@gmail.com>
2021-04-22 15:19:13 +0200

    minor changes

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import eel
 import skimage.io
 from skimage import io
 from io import BytesIO
-from skimage.color import rgb2hsv
+from skimage.color import rgb2hsv, rgb2yuv
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import base64
 import re
+
 
 eel.init("web")
 
@@ -34,10 +35,12 @@ def link(self):
     while self is None:
         pass
     else:
+        print("fired link")
         global link_to_image
         link_to_image = self
         link_to_image = decode(link_to_image)
         print("link to the image here: ", link_to_image)
+#CONVERTING FUNCTIONS ##################################################
 
 def r2h():
     while link_to_image is None:
@@ -54,11 +57,7 @@ def r2h():
         hue_64 = encode(hue)
         value_64 = encode(value)
         sat_64 = encode(sat)
-        #convert to uint8
-       # io.imsave("./web/src/imgs/converted/hue.jpg",hue)
-       # io.imsave("./web/src/imgs/converted/val.jpg",value)
-     #   io.imsave("./web/src/imgs/converted/sat.jpg",sat)
-     #   io.imsave("./web/src/imgs/converted/hsv.jpg",hsv)
+
         list64 = {
         "HSV":[hsv_64],
         "Hue":[hue_64],
@@ -68,14 +67,82 @@ def r2h():
 
         return list64
 
+
+def r2cmyk():
+
+    while link_to_image is None:
+        pass
+    else:
+        pass
+
+
+def r2yuv():
+    while link_to_image is None:
+        pass
+    else:
+        image = link_to_image
+        yuv = rgb2yuv(image)
+        y = yuv[:,:,0]
+        u = yuv[:,:,2]
+        v = yuv[:,:,1]
+
+        y_64 = encode(y)
+        u_64 = encode(u)
+        v_64 = encode(v)
+        yuv_64 = encode(yuv)
+        list64 = { #list of base64 images
+        "Luminance(Y)":[y_64],
+        "Bandwidth(U)":[u_64],
+        "Chrominance(V)":[v_64],
+        "YUV":[yuv_64]
+        }
+        return list64
+
+
+def r2r(): #rgb to components
+    while link_to_image is None:
+        pass
+    else:
+        image = link_to_image
+        imagegreen = image.copy()
+        imagegreen[:,:,0] = 0
+        imagegreen[:,:,2] = 0
+
+        imageblue = image.copy()
+        imageblue[:,:,0] = 0
+        imageblue[:,:,1] = 0
+
+        imagered = image.copy()
+        imagered[:,:,1] = 0
+        imagered[:,:,2] = 0
+
+        red_64 = encode(imagered)
+        green_64 = encode(imagegreen)
+        blue_64 = encode(imageblue)
+        list64 = { #list of base64 images
+        "Red":[red_64],
+        "Green":[green_64],
+        "Blue":[blue_64],
+        }
+
+        return list64
+
+###########################################################################
+
 dispatcher = {
     'rgb2hsv':r2h,
+    'rgb2cmyk':r2cmyk,
+    'rgb2yuv':r2yuv,
+    'rgb2rgb':r2r,
 }
 
-@eel.expose
-def passImages():
-    print("fired passImages ",)
-    return r2h()
+
+
+#@eel.expose
+#def passImages(dispatcher):
+   # print("fired passImages ",)
+
+  #  return r2h()
 
 @eel.expose
 def which_fun(self):
@@ -83,6 +150,7 @@ def which_fun(self):
         pass
     else:
         print("fired typeof: ", self)
-        dispatcher[self]()
+        #dispatcher[self]()
+        return dispatcher[self]()
 
 eel.start('index.html', size=(1200, 720))
--- ycbcr.py
+++
@@ -1,9 +0,0 @@
-from skimage import io
-from PIL import Image
-import numpy as np
-
-rgb = io.imread('./web/src/imgs/lena.jpg')
-r = rgb[:,:,0]
-y = np.zeros_like(r)
-Cb = np.zeros_like(r)
-Cr = np.zeros_like(r)
